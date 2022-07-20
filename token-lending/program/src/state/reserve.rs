5f014e8c320e3b60ee658e901197507471261bf5
Gab <100351254+gabRiel-bit20@users.noreply.github.com>
2022-07-20 11:30:57 +0800

    Fixed settle amount to always ceil. (#67)

--- token-lending/program/src/state/reserve.rs
+++ token-lending/program/src/state/reserve.rs
@@ -253,7 +253,7 @@ impl Reserve {
                 }
                 Ordering::Less => {
                     let withdraw_pct = liquidation_value.try_div(collateral.market_value)?;
-                    repay_amount = max_amount.try_floor_u64()?;
+                    repay_amount = max_amount.try_ceil_u64()?;
                     withdraw_amount = Decimal::from(collateral.deposited_amount)
                         .try_mul(withdraw_pct)?
                         .try_floor_u64()?;
@@ -285,7 +285,7 @@ impl Reserve {
                 Ordering::Less => {
                     let withdraw_pct = liquidation_value.try_div(collateral.market_value)?;
                     settle_amount = liquidation_amount;
-                    repay_amount = settle_amount.try_floor_u64()?;
+                    repay_amount = settle_amount.try_ceil_u64()?;
                     withdraw_amount = Decimal::from(collateral.deposited_amount)
                         .try_mul(withdraw_pct)?
                         .try_floor_u64()?;
