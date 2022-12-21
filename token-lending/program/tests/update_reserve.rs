1855dd9720146449ca4b7545f17233b7b95b88ce
gab <gw@port.finance>
2022-12-21 12:46:22 +0800

    Fixed tests.

--- token-lending/program/tests/update_reserve.rs
+++ token-lending/program/tests/update_reserve.rs
@@ -61,6 +61,8 @@ async fn test_update_reserve() {
             host_fee_percentage: 15,
         },
         deposit_staking_pool: COption::None,
+        deposit_limit: 2147483647u64,
+        borrow_limit: 2147483647u64,
     };
     let before_test_reserve = usdc_test_reserve.get_state(&mut banks_client).await;
     assert_ne!(before_test_reserve.config, new_config);
